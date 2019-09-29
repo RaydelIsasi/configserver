5848ca37bf5cb0a9049c5725be1e5f8b0ce9f2e5
Raydel C. Isasi <raydel.isasi92@gmail.com>
2019-09-29 00:53:52 -0300

    Config Server Application example

---
+++ startup.bat
@@ -0,0 +1,1 @@
+java -jar D:\Proyectos\server\target\server-0.0.1-SNAPSHOT.jar -Dspring.cloud.config.server.git.username=raydel.isasi92@gmail.com -Dspring.cloud.config.server.git.password= UchihaMadara777/
